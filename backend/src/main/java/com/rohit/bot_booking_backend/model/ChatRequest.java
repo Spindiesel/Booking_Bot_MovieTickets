9a28b319b775d7b91602a38a4e4d4d52d25cbbb4
SpinDiesel <reddyrohit029@gmail.com>
2026-05-29 14:47:51 +0530

    Fixed Error In Chat Request

--- backend/src/main/java/com/rohit/bot_booking_backend/model/ChatRequest.java
+++ backend/src/main/java/com/rohit/bot_booking_backend/model/ChatRequest.java
@@ -1,89 +1,14 @@
-package com.rohit.bot_booking_backend.service;
+package com.rohit.bot_booking_backend.model;
 
-import com.rohit.bot_booking_backend.model.BookingSession;
-import org.springframework.stereotype.Service;
+public class ChatRequest {
 
-@Service
-public class ChatService {
+    private String message;
 
-    private final BookingSession session = new BookingSession();
+    public String getMessage() {
+        return message;
+    }
 
-    public String getReply(String message) {
-
-        if (message.equalsIgnoreCase("book tickets")) {
-
-            session.setStep("MOVIE");
-
-            return "Which movie would you like to watch?";
-        }
-
-        switch (session.getStep()) {
-
-            case "MOVIE":
-
-                session.setMovie(message);
-                session.setStep("TICKETS");
-
-                return "How many tickets would you like?";
-
-            case "TICKETS":
-
-                session.setTickets(message);
-                session.setStep("DATE");
-
-                return "What date would you like?";
-
-            case "DATE":
-
-                session.setDate(message);
-                session.setStep("TIME");
-
-                return "What time would you like?";
-
-            case "TIME":
-
-                session.setTime(message);
-                session.setStep("CONFIRM");
-
-                return """
-                        Booking Summary
-
-                        Movie: %s
-                        Tickets: %s
-                        Date: %s
-                        Time: %s
-
-                        Confirm? (Yes/No)
-                        """
-                        .formatted(
-                                session.getMovie(),
-                                session.getTickets(),
-                                session.getDate(),
-                                session.getTime()
-                        );
-
-            case "CONFIRM":
-
-                if (message.equalsIgnoreCase("yes")) {
-
-                    session.setStep("NONE");
-
-                    return "Booking confirmed! 🎉";
-                }
-
-                session.setStep("NONE");
-
-                return "Booking cancelled.";
-
-            default:
-
-                return """
-                        Hello!
-
-                        Type:
-                        Book Tickets
-                        to start a booking.
-                        """;
-        }
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
