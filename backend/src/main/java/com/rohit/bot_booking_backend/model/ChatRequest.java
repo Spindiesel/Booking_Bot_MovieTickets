4a6571736bb90bb8ce93723f39c04910d9b01fc1
SpinDiesel <reddyrohit029@gmail.com>
2026-05-29 14:52:29 +0530

    Front End Needs To be Done

---
+++ backend/src/main/java/com/rohit/bot_booking_backend/model/ChatRequest.java
@@ -0,0 +1,14 @@
+package com.rohit.bot_booking_backend.model;
+
+public class ChatRequest {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
